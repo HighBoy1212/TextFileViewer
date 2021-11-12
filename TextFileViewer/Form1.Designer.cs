09f78d22592e828abc2e35c71462134dfbbbec96
lance.high <lance.high@hastings.edu>
2021-11-12 15:03:04 -0600

    Add project files.

---
+++ TextFileViewer/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace TextFileViewer {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.msMain = new System.Windows.Forms.MenuStrip();
+            this.miFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.miOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.miQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.msMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // msMain
+            // 
+            this.msMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miFile});
+            this.msMain.Location = new System.Drawing.Point(0, 0);
+            this.msMain.Name = "msMain";
+            this.msMain.Size = new System.Drawing.Size(504, 24);
+            this.msMain.TabIndex = 0;
+            this.msMain.Text = "menuStrip1";
+            // 
+            // miFile
+            // 
+            this.miFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miOpen,
+            this.miQuit});
+            this.miFile.Name = "miFile";
+            this.miFile.Size = new System.Drawing.Size(37, 20);
+            this.miFile.Text = "File";
+            // 
+            // miOpen
+            // 
+            this.miOpen.Name = "miOpen";
+            this.miOpen.Size = new System.Drawing.Size(103, 22);
+            this.miOpen.Text = "Open";
+            // 
+            // miQuit
+            // 
+            this.miQuit.Name = "miQuit";
+            this.miQuit.Size = new System.Drawing.Size(103, 22);
+            this.miQuit.Text = "Quit";
+            this.miQuit.Click += new System.EventHandler(this.miQuit_Click);
+            // 
+            // rtbText
+            // 
+            this.rtbText.Location = new System.Drawing.Point(12, 27);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.ReadOnly = true;
+            this.rtbText.Size = new System.Drawing.Size(480, 300);
+            this.rtbText.TabIndex = 1;
+            this.rtbText.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 339);
+            this.Controls.Add(this.rtbText);
+            this.Controls.Add(this.msMain);
+            this.MainMenuStrip = this.msMain;
+            this.Name = "MainForm";
+            this.Text = "Text File Viewer";
+            this.msMain.ResumeLayout(false);
+            this.msMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip msMain;
+        private System.Windows.Forms.ToolStripMenuItem miFile;
+        private System.Windows.Forms.ToolStripMenuItem miOpen;
+        private System.Windows.Forms.ToolStripMenuItem miQuit;
+        private System.Windows.Forms.RichTextBox rtbText;
+    }
+}
+
---
+++ TextFileViewer/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextFileViewer {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        private void miQuit_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+    }
+}
